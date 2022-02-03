17bc9d5e94edd9ad1aa66792bb57a9b4af67df0c
Haniel <60509166+mockcube@users.noreply.github.com>
2022-02-03 08:33:31 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,25 @@
-import tkinter as tk
-import curses
-import time
+from tkinter import *
+def callback(r,c):
+    global player
+    if player == 'X':
+        b[r][c].configure(text = 'X')
+        player = 'O'
+    else:
+        b[r][c].configure(text = 'O')
+        player = 'X'
 
-class __init__(self, window):
-  self.window = window
-  
+root = Tk()
+
+b = [[0,0,0],
+     [0,0,0],
+     [0,0,0]]
+
+for i in range(3):
+    for j in range(3):
+        b[i][j] = Button(font=('Verdana', 56), width=3, bg='yellow',
+                         command = lambda r=i, c=j: callback(r,c))
+        b[i][j].grid(row = i, column = j)
+
+player = 'X'
+
+mainloop()
